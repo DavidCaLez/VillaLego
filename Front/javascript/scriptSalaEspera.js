fd80b389a27a8d7b6108192573780c91e6d97ad1
dacal <d.clopez@alumnos.upm.es>
2025-05-24 15:00:29 +0200

    sala espera2

---
+++ Front/javascript/scriptSalaEspera.js
@@ -0,0 +1,47 @@
+// Function to check turn phase from server
+const params = window.location.pathname.split('/');
+const turnoId = params.pop();
+async function checkTurnPhase() {
+    try {
+        const response = await fetch(`/turno/fase/${turnoId}`);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        console.log('Current phase:', data.fase);
+        switch (data.fase) {
+            case 'Lectura instrucciones':
+                // Redirect to instructions page
+                window.location.href = '/turno/instrucciones';
+                break;
+            case 'Priorizacion de la pila del producto':
+                // Redirect to the prioritization page
+                break;
+            case 'Planificacion del sprint':
+                // Redirect to the sprint planning page
+                break;
+            case 'Ejecucion del sprint':
+                // Redirect to the sprint execution page
+                break;
+            case 'Revision del sprint':
+                // Redirect to the sprint review page
+                break;
+            case 'Retrospectiva del sprint':
+                // Redirect to the sprint retrospective page
+                break;
+            default:
+                break;
+        }
+
+    } catch (error) {
+        console.error('Error checking turn phase:', error);
+    }
+}
+
+
+const intervalId = setInterval(checkTurnPhase, 4000);
+
+
+window.addEventListener('unload', () => {
+    clearInterval(intervalId);
+});
